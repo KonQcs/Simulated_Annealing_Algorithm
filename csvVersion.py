3e6a92e49ad1967f3fb52c8a2ca4ec821b671922
Konstantinos Kiousis <kkiousis2023@gmail.com>
2025-07-04 15:29:27 +0300

    Create csvVersion.py

---
+++ csvVersion.py
@@ -0,0 +1,156 @@
+import math
+import random
+from collections import namedtuple
+import csv
+
+
+def generate_NPE(n):
+    block_ids = [str(i) for i in range(1, n + 1)]
+    operators = ['H', 'V']
+    expr = []
+    stack_depth = 0
+
+    random.shuffle(block_ids)  # ανακάτεμα των blocks
+
+    while block_ids or stack_depth > 1:
+        # πάντα το πρώτο token πρέπει να είναι block
+        if block_ids and (stack_depth < 2 or random.random() < 0.7):
+            expr.append(block_ids.pop())
+            stack_depth += 1
+        else:
+            op = random.choice(operators)
+            expr.append(op)
+            stack_depth -= 1  # 2 στοιχεία γίνονται 1
+    return expr
+
+def RandomMove(expr):
+    new_expr = expr.copy()
+    move_type = random.randint(1, 4)  #choose random move 1,2,3,4
+
+    if move_type == 1:
+        # Ανταλλαγή δύο τελεστών
+        i = random.randint(0, len(expr) - 2)
+        if (expr[i] in ['H', 'V'] and expr[i + 1] in ['H', 'V']) or (expr[i] not in ['H', 'V'] and expr[i + 1] not in ['H', 'V']):
+            new_expr[i], new_expr[i + 1] = new_expr[i + 1], new_expr[i]
+
+    elif move_type == 2:
+        # Αντιστροφή αλυσίδας
+        start = random.randint(0, len(expr) - 3)
+        end = random.randint(start + 2, len(expr) - 1)
+        sub = new_expr[start:end]
+        sub.reverse()
+        new_expr[start:end] = sub
+
+    elif move_type == 3:
+        #αλλαγή e(i) -- e(i+1)
+        for i in range(1, len(expr) - 1):
+            ei = expr[i]
+            if ei not in ['H', 'V']:
+                ei_prev = expr[i - 1]
+                ei_next = expr[i + 1]
+                if ei_prev != ei_next and expr.count(ei_next) * 2 < i:
+                    new_expr[i], new_expr[i + 1] = new_expr[i + 1], new_expr[i]
+                    break
+
+    elif move_type == 4:
+        #ανταλλαγή width - height
+        operand_indices = [i for i, token in enumerate(expr) if token not in ['H', 'V']]
+        if operand_indices:
+            index = random.choice(operand_indices)
+            block_id = new_expr[index]
+            original = blocks[block_id]
+            blocks[block_id] = Block(id=block_id, width=original.height, height=original.width)
+
+    return new_expr
+
+def IsValid(expr):
+    count = 0
+    for i in range(len(expr)):
+        token = expr[i]
+
+        # διαδοχικά H ή V
+        if i < len(expr) - 1:
+            if token in ['H', 'V'] and token == expr[i + 1]:
+                return False
+
+        #ισορροπίας
+        if token in ['H', 'V']:
+            count -= 1
+        else:
+            count += 1
+
+        if count < 1:
+            return False
+
+    return True
+
+def EvaluateArea(expr):
+    stack = []
+    for token in expr:
+        if token not in ['H', 'V']:
+            b = blocks[token]
+            stack.append((b.width, b.height))
+        else:
+            b2 = stack.pop()
+            b1 = stack.pop()
+            if token == 'H':
+                w = max(b1[0], b2[0])
+                h = b1[1] + b2[1]
+            else:  #if V
+                w = b1[0] + b2[0]
+                h = max(b1[1], b2[1])
+            stack.append((w, h))
+    final_w, final_h = stack[0]
+    return final_w * final_h
+
+def EvaluateP(d):
+    negd = d - 2*d
+    p = math.exp( negd / T )
+    return p
+
+#δομή block
+Block = namedtuple('Block', ['id', 'width', 'height'])
+
+#πληθος block
+n = 20
+
+#εκφραση
+E = generate_NPE(n)
+print(E, '\n')
+# x, y για καθε block
+blocks = {}
+with open('blocks.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        bid = int(row['id'])
+        blocks[str(bid)] = Block(bid, int(row['width']), int(row['height']))
+
+
+Best = E
+BestArea = EvaluateArea(E)
+#Temperture
+T = 100
+r = 0.999
+# New T after each repeat = r*T >>> 0.999*100 = 99.9
+limit =0.1
+repeats = 0
+
+while T > limit:
+    NE = RandomMove(E)
+    if IsValid(NE):
+        area = EvaluateArea(NE)
+        delta = area - BestArea
+        if delta < 0 or random.random() < EvaluateP(delta):
+            E = NE
+            if area < BestArea:
+                Best = NE
+                BestArea = area
+        T *= r
+        repeats += 1
+        if repeats > 100000:  # ασφάλεια για άπειρους βρόχους
+            break
+
+print(Best,'\t', BestArea,'\t', repeats)
+
+with open("results.txt", "a") as file:
+    file.write(f"{Best}\t{BestArea}\t{repeats}\n")
